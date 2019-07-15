cdf72afe7034cfc91fdbed56da104a2cab6ece88
dimitarmileski <dimit@r.com>
2019-07-15 13:34:16 +0200

    Video Model
    getVideoMetadata
    cutVideo

--- YoDo/Models/Video.cs
+++ YoDo/Models/Video.cs
@@ -28,24 +28,25 @@ namespace YoDo.Models
         public static void DownloadVideoThumb(string inputName, string outputName)
         {
 
-            var inputFile = new MediaFile { Filename = homePath +  @"\Downloads\" + inputName + ".mp4" };
-            var outputFile = new MediaFile { Filename = homePath +  @"\Downloads\"+ outputName +".jpg" };
+            var inputFile = new MediaFile { Filename = homePath + @"\Downloads\" + inputName + ".mp4" };
+            var outputFile = new MediaFile { Filename = homePath + @"\Downloads\" + outputName + ".jpg" };
 
             using (var engine = new Engine())
             {
                 engine.GetMetadata(inputFile);
 
-              //Saves the frame located on the 15th second of the video.
-               var options = new ConversionOptions { Seek = TimeSpan.FromSeconds(15) };
+                //Saves the frame located on the 15th second of the video.
+                var options = new ConversionOptions { Seek = TimeSpan.FromSeconds(15) };
                 engine.GetThumbnail(inputFile, outputFile, options);
             }
 
         }
 
-        public static void Convert(string inputName ,string inputFormat, string outputName ,string outputFormat) {
+        public static void Convert(string inputName, string inputFormat, string outputName, string outputFormat)
+        {
 
-            var inputFile = new MediaFile { Filename = homePath +  @"\Downloads\"+ inputName + "." + inputFormat };
-            var outputFile = new MediaFile { Filename =homePath +  @"\Downloads\" + outputName + "." + outputFormat };
+            var inputFile = new MediaFile { Filename = homePath + @"\Downloads\" + inputName + "." + inputFormat };
+            var outputFile = new MediaFile { Filename = homePath + @"\Downloads\" + outputName + "." + outputFormat };
 
             using (var engine = new Engine())
             {
@@ -53,9 +54,10 @@ namespace YoDo.Models
             }
         }
 
-        public static void convertToDVD(string inputName, string outputName) {
+        public static void convertToDVD(string inputName, string outputName)
+        {
 
-            var inputFile = new MediaFile { Filename = homePath +  @"\Downloads\" + inputName + ".mp4" };
+            var inputFile = new MediaFile { Filename = homePath + @"\Downloads\" + inputName + ".mp4" };
             var outputFile = new MediaFile { Filename = homePath + @"\Downloads\" + outputName + ".vob" };
 
             var conversionOptions = new ConversionOptions
@@ -71,9 +73,10 @@ namespace YoDo.Models
 
         }
 
-        public static void ConvertOptions(string inputName, string outputName) {
-            var inputFile = new MediaFile { Filename = homePath +  @"\Downloads\" + inputName+ ".mp4" };
-            var outputFile = new MediaFile { Filename = homePath +  @"\Downloads\" + outputName + ".mp4" };
+        public static void ConvertOptions(string inputName, string outputName)
+        {
+            var inputFile = new MediaFile { Filename = homePath + @"\Downloads\" + inputName + ".mp4" };
+            var outputFile = new MediaFile { Filename = homePath + @"\Downloads\" + outputName + ".mp4" };
 
             var conversionOptions = new ConversionOptions
             {
@@ -90,15 +93,43 @@ namespace YoDo.Models
 
         }
 
-        public static void getVideoMetadata() {
+        public static void getVideoMetadata(string inputName)
+        {
+            var inputFile = new MediaFile { Filename = homePath + @"\Downloads\" + inputName };
+            //METADATA
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+            }
 
+            System.Diagnostics.Debug.WriteLine(inputFile.Metadata.Duration);
 
         }
 
+        public static void cutVideo(string inputName, string outputName, int startingSec, int duration)
+        {
+
+            var inputFile = new MediaFile { Filename = homePath + @"\Downloads\" + inputName };
+            var outputFile = new MediaFile { Filename = homePath + @"\Downloads\" + outputName };
+
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+
+                var options = new ConversionOptions();
+
+                // This example will create a 25 second video, starting from the 
+                // 30th second of the original video.
+                //// First parameter requests the starting frame to cut the media from.
+                //// Second parameter requests how long to cut the video.
+                options.CutMedia(TimeSpan.FromSeconds(startingSec), TimeSpan.FromSeconds(duration));
+
+                engine.Convert(inputFile, outputFile, options);
+
+            }
 
 
-
+        }
     }
-
    
 }
